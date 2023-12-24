f83c10126777942a1d5d0748881b852437966912
Nitish Anand <nitish4u029@gmail.com>
2023-12-24 11:03:45 +0530

    sw addAll issue fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,52 @@
 const CACHE_NAME = "TikTok-clone-cache-v1";
 const DYNAMIC_CACHE_NAME = "TikTok-clone-dynamic-cache";
-const CACHE_EXPIRATION_DAYS = 7; // Set the cache expiration period in days
-const CACHE_EXPIRATION_SECONDS = CACHE_EXPIRATION_DAYS * 24 * 60 * 60; // Convert days to seconds
+const CACHE_EXPIRATION_DAYS = 7;
+const CACHE_EXPIRATION_SECONDS = CACHE_EXPIRATION_DAYS * 24 * 60 * 60;
 
-// const staticAssets = ["/"];
+const staticAssets = [
+  "/",
+  "/index.html",
+  "/static/css/main.chunk.css",
+  "/static/js/bundle.js",
+  "/static/js/0.chunk.js",
+  "/static/js/1.chunk.js",
+  "/favicon.ico",
+  "/static/media/tiktok-logo-small.8fb50fcb18e1f9bc7082.webp",
+  "/static/media/tiktok-logo.0148e250a3e768af5ea2.webp",
+  "/manifest.json",
+  "/proxy/https://randomuser.me/api/portraits/men/1.jpg",
+  "/proxy/https://randomuser.me/api/portraits/women/2.jpg",
+  "/proxy/https://randomuser.me/api/portraits/women/3.jpg",
+  "/proxy/https://randomuser.me/api/portraits/men/4.jpg",
+  "/proxy/https://randomuser.me/api/portraits/women/5.jpg",
+  "/proxy/https://randomuser.me/api/portraits/men/6.jpg",
+  "/proxy/https://randomuser.me/api/portraits/men/7.jpg",
+  "/proxy/https://randomuser.me/api/portraits/women/8.jpg",
+  "/proxy/https://randomuser.me/api/portraits/men/9.jpg",
+  "/proxy/https://randomuser.me/api/portraits/women/10.jpg",
+];
 
 let deferredPrompt;
 
 self.addEventListener("install", (event) => {
-  event
-    .waitUntil
-    // caches.open(CACHE_NAME).then((cache) => {
-    //   return cache.addAll(staticAssets);
-    // })
-    ();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return Promise.all(
+        staticAssets.map((url) => {
+          return fetch(url)
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error(`Failed to fetch: ${url}`);
+              }
+              return cache.put(url, response);
+            })
+            .catch((error) => {
+              console.error(`Failed to fetch and cache: ${url}`, error);
+            });
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener("activate", (event) => {
@@ -31,28 +64,23 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", (event) => {
   const request = event.request;
 
-  // First, handle caching logic
   event.respondWith(
-    fetch(request)
-      .then(async (response) => {
-        // Check if the response is partial before caching
-        if (response.status === 200 && response.ok) {
-          const cache = await caches.open(DYNAMIC_CACHE_NAME);
-          cache.put(request, response.clone());
-        }
-
-        return response;
-      })
-      .catch(async () => {
-        const cachedResponse = await caches.match(request);
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return new Response("Offline content not available.");
-      })
+    caches.match(request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(request).then((response) => {
+          if (response.ok) {
+            const cacheCopy = response.clone();
+            caches.open(DYNAMIC_CACHE_NAME).then((cache) => {
+              cache.put(request, cacheCopy);
+            });
+          }
+          return response;
+        })
+      );
+    })
   );
 
-  // Hide the splash screen once the content is loaded
   event.waitUntil(
     (async function () {
       const response = await fetch(request);
@@ -65,7 +93,18 @@ self.addEventListener("fetch", (event) => {
   );
 });
 
-// Periodically clean up old caches and set cache expiration
+self.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  showInstallPrompt();
+});
+
+function showInstallPrompt() {
+  console.log("Show custom install prompt UI");
+  // Implement your custom UI logic here
+}
+
+// Cleanup old caches and set cache expiration
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -77,25 +116,3 @@ self.addEventListener("activate", (event) => {
     })
   );
 });
-
-// Handle installation prompt
-self.addEventListener("beforeinstallprompt", (event) => {
-  // Prevent the default prompt
-  event.preventDefault();
-
-  // Store the event for later use
-  deferredPrompt = event;
-
-  // Optionally, show your own custom UI to prompt the user
-  // (e.g., a button that says "Add to Home Screen")
-
-  // You can customize the UI as needed
-  // Example: show a button or display a custom popup
-  showInstallPrompt();
-});
-
-// Custom function to show installation prompt
-function showInstallPrompt() {
-  // Implement your custom UI logic here
-  console.log("Show custom install prompt UI");
-}
